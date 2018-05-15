cb5c6d0bd974d40e521e95f5c812c0de4b738ef8
rob-crane <rob-crane@users.noreply.github.com>
2018-05-14 20:19:13 -0400

    optimizes but suspect x,y need to be swapped

--- thresholding.py
+++ thresholding.py
@@ -41,7 +41,7 @@ class LaneImage:
                 bool_img)
         return bool_img
 
-    def get_lanes(self, save_img = False):
+    def top_view(self, save_img = False):
         bool_img = (self.sat_mag(config.SMAG_THRESHOLD) & \
                 self.sat_direc(config.SDIR_THRESHOLD)) | \
                 (self.lght_mag(config.LMAG_THRESHOLD) & \
@@ -75,6 +75,82 @@ class LaneImage:
             LaneImage.__bool_imwrite('pipeline/' + self.name + '_msk.png', masked_img)
             LaneImage.__bool_imwrite('pipeline/' + self.name + '_top.png', top_view)
         return top_view
+
+    def lane_poly(self, n):
+
+        # calculates estimated left lane position
+        def get_y_prime(p, x):
+            ret = np.zeros(len(x))
+            for i in range(len(p)):
+                ret = ret + p[i] * x**i
+            
+            return ret
+
+        # deltas between a hot pixel and a candidate left and right lane polynomial
+        def get_deltas(y_prime, y, lane_width):
+            return np.array([(y-y_prime), (y + lane_width - y_prime)]).T
+
+        def get_loss(deltas):
+            candidate_losses = deltas**2
+            min_ind = np.argmin(candidate_losses, axis=1)
+            m = len(deltas)
+            loss = np.sum(candidate_losses.take(min_ind)) / m / 2
+            closer_lane = np.where(min_ind == 0, 'l', 'r').T
+
+            return loss, closer_lane
+
+        def get_grads(closer_lanes, deltas, x, r):
+            min_ind = np.where(closer_lanes == 'l', 0, 1)
+            closer_delta = deltas.take(min_ind)
+            grads = []
+            for i in range(r):
+                grads.append(closer_delta * x**i)
+            return np.sum(grads, axis=1) / deltas.shape[0]
+
+        top_view = self.top_view()
+        lane_px = np.argwhere(top_view)
+        # now zero-center + normalize (convert to z-scores)
+        means = np.mean(lane_px, axis=0)
+        devs = np.std(lane_px, axis=0)
+        import pdb; pdb.set_trace()
+        norm_px = (lane_px - means) / devs
+        
+        x,y = norm_px[:,0], norm_px[:,1]
+        p = np.random.rand(n) # randomly initialize polynomial coefficients
+        def loss_fn(p):
+            y_prime = get_y_prime(p, x)
+            deltas = get_deltas(y_prime, y, 75/devs[1])
+            loss, closer_lane = get_loss(deltas)
+            return loss
+
+        # def grad_fn(p):
+            # y_prime = get_y_prime(p, x)
+            # deltas = get_deltas(y_prime, y, 75)
+            # _, closer_lane = get_loss(deltas)
+            # return get_grads(closer_lane,  deltas, x, n)
+
+
+        print('----------\nnaive loss:' , loss_fn(p))
+        from scipy.optimize import minimize
+        res = minimize(loss_fn, p, method='Nelder-Mead',
+            options={'disp': True, 'xatol':0.0000000001})
+            # jac=grad_fn, options={'disp': True})
+
+        draw_x = np.arange(0, self.img.shape[0]) 
+        draw_x_norm = (draw_x - means[0]) / devs[0]
+        draw_y_norm = get_y_prime(res.x, 
+                draw_x_norm)
+        draw_y = draw_y_norm * devs[1] + means[1]
+
+        draw_y2 = draw_y + 75
+        pts = np.int32([list(zip(draw_x, draw_y))])
+        pts2 = np.int32([list(zip(draw_x, draw_y2))])
+        # import pdb; pdb.set_trace()
+        lines = cv2.polylines(top_view, pts=pts[:,:,1::-1], isClosed=False, color=1)
+        lines = cv2.polylines(lines, pts=pts2[:,:,1::-1], isClosed=False, color=1)
+        LaneImage.__bool_imwrite('pipeline/' + self.name + '_ply.png', lines)
+
+        return res.x
 
     # copied from Udacity CarND Project1
     def _region_of_interest(img, vertices):
@@ -134,25 +210,12 @@ def thresholds(begin, end, delta):
         if begin >= end:
             break
 
+
 if __name__ == '__main__':
 
     img_files = glob.glob('test_images/*.jpg')
     for fname in img_files:
         img = LaneImage(fname)
-        img.get_lanes(True)
+        img.lane_poly(4)
 
-        # lmag_thresholds = thresholds(0, 140000, 7000)
-        # for threshold in lmag_thresholds:
-            # img.lght_mag(threshold, True) 
 
-        # smag_thresholds = thresholds(0, 170000, 8500)
-        # for threshold in smag_thresholds:
-            # img.sat_mag(threshold, True) 
-
-        # sdir_thresholds = thresholds(0, 1.6, 0.08)
-        # for threshold in sdir_thresholds:
-            # img.sat_direc(threshold, True) 
-
-        # ldir_thresholds = thresholds(0, 1.6, 0.08)
-        # for threshold in ldir_thresholds:
-            # img.lght_direc(threshold, True) 
