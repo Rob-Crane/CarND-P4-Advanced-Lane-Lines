bdd4b4f234254b712b93885790d3a1c7d5faedc3
rob-crane <rob-crane@users.noreply.github.com>
2018-05-26 23:55:04 -0400

    adding missing images

--- images/resize.py
+++ images/resize.py
@@ -1,7 +1,7 @@
 import glob
 import cv2
 
-for fname in glob.glob('bad_convergence.png'):
+for fname in glob.glob('test*png'):
     img = cv2.imread(fname)
     dst = cv2.resize(img, (0,0), fx=0.3, fy=0.3)
     cv2.imwrite('r_' + fname, dst)
